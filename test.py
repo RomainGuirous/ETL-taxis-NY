1d40a0701735436035e76e26d685e98b5bcf3862
RomainGuirous <romaing_59@hotmail.fr>
2025-03-28 01:18:19 +0100

    download script

---
+++ test.py
@@ -0,0 +1,4 @@
+import download_script as ds
+
+# on appelle la fonction download_yellow_taxis_files_years de download_script.py pour télécharger les fichiers parquet des yellow taxis de 2019 et 2020
+ds.download_yellow_taxis_files_years(2019, 2019) # télécharge les fichiers parquet des yellow taxis de 2019 et 2020
